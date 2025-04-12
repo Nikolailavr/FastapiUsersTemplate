19f5f00f7e67623aaa6656674469f423085301ad
Nikolai Lavrov <mail@nlavrov.ru>
2025-04-13 01:31:26 +0700

    add SessionDep

--- src/core/dependencies/authentication/access_token.py
+++ src/core/dependencies/authentication/access_token.py
@@ -1,21 +1,6 @@
-from typing import (
-    TYPE_CHECKING,
-    Annotated,
-)
-
-from fastapi import Depends
-
-from core.db import db_helper
+from core.dependencies import SessionDep
 from core.models import AccessToken
 
-if TYPE_CHECKING:
-    from sqlalchemy.ext.asyncio import AsyncSession
 
-
-async def get_access_tokens_db(
-    session: Annotated[
-        "AsyncSession",
-        Depends(db_helper.session_getter),
-    ],
-):
+async def get_access_tokens_db(session: SessionDep):
     yield AccessToken.get_db(session=session)
--- src/core/dependencies/authentication/user.py
+++ src/core/dependencies/authentication/user.py
@@ -1,21 +1,6 @@
-from typing import (
-    TYPE_CHECKING,
-    Annotated,
-)
-
-from fastapi import Depends
-
-from core.db import db_helper
+from core.dependencies import SessionDep
 from core.models import User
 
-if TYPE_CHECKING:
-    from sqlalchemy.ext.asyncio import AsyncSession
 
-
-async def get_users_db(
-    session: Annotated[
-        "AsyncSession",
-        Depends(db_helper.session_getter),
-    ],
-):
+async def get_users_db(session: SessionDep):
     yield User.get_db(session=session)
